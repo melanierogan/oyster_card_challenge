c613e5501f4e370529c449e166fd8a88e1eadd78
Melanie Rogan <melanie.rogan@ft.com>
2019-05-20 14:53:03 +0100

    step 4 completed

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,3 +1,12 @@
 class Oystercard
 
+    attr_reader :balance
+    
+    def initialize
+        @balance = 0
+    end
+
+    # def balance 
+    #     return 0
+    # end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,4 +1,9 @@
 require './lib/Oystercard'
 
 describe Oystercard do
+    it 'gives you a balance of 0' do
+        oystercard = Oystercard.new
+        oystercard.balance
+        expect(oystercard.balance).to eq(0)
+    end
 end
