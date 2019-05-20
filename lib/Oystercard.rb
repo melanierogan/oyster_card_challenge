f290864457c68f9ad29a37ea410e9aa2e1b67b07
Melanie Rogan <melanie.rogan@ft.com>
2019-05-20 15:51:47 +0100

    finishing step 6 using DONT REPEAT YOURSELF

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,14 +1,19 @@
 class Oystercard
 
-    attr_reader :balance
-
-    def initialize
+    attr_reader :balance, :total
+    attr_accessor :limit
+    
+    DEFAULT = 90
+    def initialize(limit = DEFAULT)
         @balance = 0
+        @total
+        @limit = limit
+
     end
 
     def top_up(num)
-      balance + num
+      total = balance + num 
+      total > limit ? raise("maximum balance reached #{limit}!") : total
     end
-
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -7,9 +7,19 @@ describe Oystercard do
         expect(oystercard.balance).to eq(0)
     end
 
+
+
+end
+
+describe "top_up" do
     it 'can be topped up' do
-      oystercard = Oystercard.new
-      oystercard.top_up(50)
-      expect(oystercard.top_up(50)).to eq(50)
-    end
+        oystercard = Oystercard.new
+        oystercard.top_up(50)
+        expect(oystercard.top_up(50)).to eq(50)
+      end
+  
+      it 'throws an error when 90 quid limit is reached' do
+          oystercard = Oystercard.new
+          expect{oystercard.top_up(91)}.to raise_error("maximum balance reached #{Oystercard::DEFAULT}!")
+      end
 end
